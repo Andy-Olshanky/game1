ae32c885cf1b27d8c75ae5fcc99b615874775777
Andy <olshansa@gmail.com>
2024-01-27 14:55:20 -0500

    put a square on the screen

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,45 @@
-use ggez::{event::EventHandler, graphics::{self, Canvas}, Context, GameResult};
+use ggez::{
+    event::EventHandler,
+    graphics::{self, Canvas, Color, DrawParam, Rect},
+    Context, GameResult,
+};
 
-pub struct GameState {}
+pub const SCREEN_SIZE: (f32, f32) = (1000.0, 700.0);
+
+pub struct GameState {
+    square: Rect,
+}
 
 impl GameState {
     pub fn new() -> GameResult<Self> {
-        Ok(GameState {})
+        let square_length = 50.0;
+        let square = Rect::new(
+            SCREEN_SIZE.0 / 2.0 - square_length / 2.0,
+            SCREEN_SIZE.1 / 4.0,
+            square_length,
+            square_length,
+        );
+
+        Ok(GameState { square })
     }
 }
 
 impl EventHandler for GameState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        while ctx.time.check_update_time(1) {
-            
-        }
+        while ctx.time.check_update_time(1) {}
 
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let canvas = Canvas::from_frame(ctx, graphics::Color::BLACK);
+        let mut canvas = Canvas::from_frame(ctx, graphics::Color::BLACK);
+
+        canvas.draw(
+            &graphics::Quad,
+            DrawParam::new()
+            .dest_rect(self.square)
+            .color(Color::WHITE),
+        );
 
         canvas.finish(ctx)?;
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use game1::GameState;
+use game1::{GameState, SCREEN_SIZE};
 use ggez::{event, ContextBuilder, GameResult};
 
 fn main() -> GameResult {
     let (ctx, event_loop) = ContextBuilder::new("first game", "Andy Olshansky")
-        .window_mode(ggez::conf::WindowMode::default().dimensions(1000.0, 700.0))
+        .window_mode(ggez::conf::WindowMode::default().dimensions(SCREEN_SIZE.0, SCREEN_SIZE.1))
         .window_setup(ggez::conf::WindowSetup::default().title("First Game :)"))
         .build()?;
 
