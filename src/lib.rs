752c90dffd3a0a84dfec2334621d34fa7a67d417
Andy <olshansa@gmail.com>
2024-01-27 13:15:48 -0500

    We have a blank screen, let's begin

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,26 @@
 use ggez::{event::EventHandler, graphics::{self, Canvas}, Context, GameResult};
 
-pub struct GameState {
-    inc: i32,
-}
+pub struct GameState {}
 
 impl GameState {
-    pub fn new(ctx: &mut Context) -> GameResult<Self> {
-        Ok(GameState {
-            inc: 0
-        })
-    }
-
-    pub fn draw_thing(&mut self, ctx: &mut Context) -> GameResult {
-        let mut canvas: Canvas;
-        if self.inc % 2 == 1 {
-            canvas = Canvas::from_frame(ctx, graphics::Color::BLACK);
-        }
-        else {
-            canvas = Canvas::from_frame(ctx, graphics::Color::WHITE);
-        }
-
-        canvas.finish(ctx)?;
-
-        Ok(())
+    pub fn new() -> GameResult<Self> {
+        Ok(GameState {})
     }
 }
 
 impl EventHandler for GameState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
         while ctx.time.check_update_time(1) {
-            self.inc += 1;
+            
         }
 
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        self.draw_thing(ctx)?;
+        let canvas = Canvas::from_frame(ctx, graphics::Color::BLACK);
+
+        canvas.finish(ctx)?;
 
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use game1::GameState;
 use ggez::{event, ContextBuilder, GameResult};
 
 fn main() -> GameResult {
-    let (mut ctx, event_loop) = ContextBuilder::new("first game", "Andy Olshansky")
+    let (ctx, event_loop) = ContextBuilder::new("first game", "Andy Olshansky")
         .window_mode(ggez::conf::WindowMode::default().dimensions(1000.0, 700.0))
         .window_setup(ggez::conf::WindowSetup::default().title("First Game :)"))
         .build()?;
 
-    let state = GameState::new(&mut ctx)?;
+    let state = GameState::new()?;
 
     event::run(ctx, event_loop, state);
 }
