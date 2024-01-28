9f7f9039a05282b0dc0c2be6bde0d62022176e90
Andy <olshansa@gmail.com>
2024-01-27 19:23:24 -0500

    figured out how to draw a line lets gooooooo

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,53 @@
 use ggez::{
     event::EventHandler,
     graphics::{self, Canvas, Color, DrawParam, Rect},
+    mint::Point2,
     Context, GameResult,
 };
 
 pub const SCREEN_SIZE: (f32, f32) = (1000.0, 700.0);
 
-pub struct GameState {
+struct Square {
     square: Rect,
+}
+
+impl Square {
+    fn new() -> Square {
+        let length = 50.0;
+
+        Square {
+            square: Rect::new(
+                SCREEN_SIZE.0 / 2.0 - length / 2.0,
+                SCREEN_SIZE.1 / 8.0,
+                length,
+                length,
+            ),
+        }
+    }
+
+    fn move_position(&mut self, x: f32, y: f32) {
+        self.square.x += x;
+        self.square.y += y;
+    }
+}
+
+pub struct GameState {
+    square: Square,
 }
 
 impl GameState {
     pub fn new() -> GameResult<Self> {
-        let square_length = 50.0;
-        let square = Rect::new(
-            SCREEN_SIZE.0 / 2.0 - square_length / 2.0,
-            SCREEN_SIZE.1 / 4.0,
-            square_length,
-            square_length,
-        );
-
-        Ok(GameState { square })
+        Ok(GameState {
+            square: Square::new(),
+        })
     }
 }
 
 impl EventHandler for GameState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        while ctx.time.check_update_time(1) {}
+        while ctx.time.check_update_time(60) {
+            self.square.move_position(0.0, 5.0);
+        }
 
         Ok(())
     }
@@ -37,9 +58,17 @@ impl EventHandler for GameState {
         canvas.draw(
             &graphics::Quad,
             DrawParam::new()
-            .dest_rect(self.square)
-            .color(Color::WHITE),
+                .dest_rect(self.square.square)
+                .color(Color::WHITE),
         );
+
+        let start_point = Point2 { x: 0.0, y: 600.0 };
+        let end_point = Point2 { x: 1000.0, y: 600.0 };
+        let line_thickness = 1.0;
+        let line_color = Color::new(1.0, 1.0, 1.0, 1.0);
+        let line_mesh = graphics::Mesh::new_line(ctx, &[start_point, end_point], line_thickness, line_color)?;
+
+        canvas.draw(&line_mesh, graphics::DrawParam::default());
 
         canvas.finish(ctx)?;
 
